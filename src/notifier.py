aef33e5c0bdada6b977b233bd8d31b27545550db
Riboshom <riboshom@hotmail.com>
2015-03-14 22:50:20 -0400

    Too mad after python to bother to do actual modules ATM

--- src/notifier.py
+++ src/notifier.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env/python3
 import sys
-import subprocess
 import time
 import json
 import getopt
+import requests
+import subprocess
 
-def query_server(game_id):
-    data = {}
-    command = "ip addr | grep 'inet ' | awk '{print $2}' | cut -d/ -f1 | grep -v '127.' | head -n 1"
-    data['lan_ip'] = subprocess.check_output(command, shell=True).rstrip().decode()
-    data['game_id'] = game_id
-    return data
+class Protocol:
+    def query_server(self, hostname, port):
+        data = {}
+        command_ip = "ip addr | grep 'inet ' | awk '{print $2}' | cut -d/ -f1 | grep -v '127.' | head -n 1"
+        data['hostname'] = subprocess.check_output("hostname", shell=True).rstrip().decode()
+        data['lan_ip'] = subprocess.check_output(command_ip, shell=True).rstrip().decode()
+        return data
+
+class GameSpy(Protocol):
+    pass
+
+class Main:
+    protocols = {
+        "gamespy": GameSpy,
+        "base": Protocol,
+    }
 
 def notify_master(hostname, data):
     payload = json.dumps(data)
@@ -19,11 +30,13 @@ def notify_master(hostname, data):
 
 # python3 notifier.py --delay=30 --notify=lanmomo.ca css
 def main():
-    opts, args = getopt.getopt(sys.argv[1:], 'r:n:s', ['repeat=', 'notify=', 'show'])
+    opts, args = getopt.getopt(sys.argv[1:], 'r:n:sP:p:', ['repeat=', 'notify=', 'show', 'protocol=', 'port='])
 
-    show_data=False
-    hostname=False
-    repeat_delay=False
+    show_data=None
+    hostname=None
+    repeat_delay=None
+    protocol_name="Base"
+    port=None
 
     for opt in opts:
         if opt[0] in ('-n', '--notify'):
@@ -32,14 +45,26 @@ def main():
             repeat_delay=int(opt[1])
         elif opt[0] in ('-s', '--show'):
             show_data=True
+        elif opt[0] in ('-P', '--protocol'):
+            protocol_name=opt[1]
+        elif opt[0] in ('-p', '--port'):
+            port=opt[1]
 
     if not args:
         print('No game_id specified')
         exit()
     game_id = args[0]
 
+    try:
+        protocol_class = Main.protocols[protocol_name.lower()]
+    except IndexError:
+        protocol_class = Protocol()
+    p = protocol_class()
+
     while True:
-        query_result = query_server(game_id)
+        query_result = p.query_server("localhost", port)
+        query_result['game_id'] = game_id
+
         if show_data:
             print(query_result)
         if hostname:
