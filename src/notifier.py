006caf5cdf36f17e84c4ad8e9b9ea32d183bcf07
Justin Duplessis <drfoliberg@gmail.com>
2015-03-14 20:22:42 -0400

    Add hostname to server info

--- src/notifier.py
+++ src/notifier.py
@@ -7,8 +7,9 @@ import getopt
 
 def query_server(game_id):
     data = {}
-    command = "ip addr | grep 'inet ' | awk '{print $2}' | cut -d/ -f1 | grep -v '127.' | head -n 1"
-    data['lan_ip'] = subprocess.check_output(command, shell=True).rstrip().decode()
+    command_ip = "ip addr | grep 'inet ' | awk '{print $2}' | cut -d/ -f1 | grep -v '127.' | head -n 1"
+    data['hostname'] = subprocess.check_output("hostname", shell=True).rstrip().decode()
+    data['lan_ip'] = subprocess.check_output(command_ip, shell=True).rstrip().decode()
     data['game_id'] = game_id
     return data
 
