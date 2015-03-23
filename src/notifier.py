011ff723137849a796c30cb31035386b8fa594f5
Riboshom <riboshom@hotmail.com>
2015-03-23 01:04:33 -0400

    Default protocol list and changed args parsing to support multiple games

--- src/notifier.py
+++ src/notifier.py
@@ -11,16 +11,23 @@ def notify_master(hostname, data):
     payload = json.dumps(data)
     requests.post(hostname, data=payload)
 
+def query_server(protocol_name, port):
+    if protocol_name == "gamespy":
+        query_method = protocols.gamespy_query_server
+    elif protocol_name in ("a2s", "source"):
+        query_method = protocols.a2s_query_server
+    else :
+        query_method = protocols.network_identity
 
-# python3 notifier.py --delay=30 --notify=lanmomo.ca css
+    return query_method(port) # Server is always the current machine
+
+# python3 notifier.py -r 30 -n lanmomo.ca css dod:a2s:27015
 def main():
-    opts, args = getopt.getopt(sys.argv[1:], 'r:n:sP:p:', ['repeat=', 'notify=', 'show', 'protocol=', 'port='])
+    opts, args = getopt.getopt(sys.argv[1:], 'r:n:s', ['repeat=', 'notify=', 'show'])
 
     show_data = None
     hostname = None
     repeat_delay = None
-    protocol_name = "None"
-    port=None
 
     for opt in opts:
         if opt[0] in ('-n', '--notify'):
@@ -29,34 +36,42 @@ def main():
             repeat_delay = int(opt[1])
         elif opt[0] in ('-s', '--show'):
             show_data = True
-        elif opt[0] in ('-P', '--protocol'):
-            protocol_name = opt[1]
-        elif opt[0] in ('-p', '--port'):
-            port=opt[1]
 
-    if not args:
-        print('No game_id specified')
+    games_on_server = [];
+
+    for arg in args:
+        # ut2004:gamespy:1337
+        parts = arg.split(':')
+        game_id = parts[0]
+        if len(parts) == 1 :
+            protocol = protocols.default_game_settings.get(game_id)
+            if protocol == None :
+                print("%s does not have a pre-assigned protocol." % game_id)
+                continue
+        elif len(parts) == 3 and isinstance(parts[1], str) and isinstance(parts[2], int):
+            protocol = (parts[1].lower, parts[2])
+        game_protocol_pair = (game_id, protocol)
+        games_on_server.append(game_protocol_pair)
+
+    if not games_on_server:
+        print('No valid game specified')
         exit(1)
-    game_id = args[0]
-
-    # What protocol does Trackmania uses?
-    # Call of Duty 4 uses Quake 3 protocol
-
-    if protocol_name.lower() == "gamespy": # Unreal, GMOD, Minecraft (1.8+)
-        query_server = protocols.gamespy_query_server
-    elif protocol_name.lower() in ("a2s", "source"): # CSS, TF2, Natural Selection, CSGO,
-        query_server = protocols.a2s_query_server
-    else :
-        query_server = protocols.network_identity
 
     while True:
-        query_result = query_server(port) # Server is always the current machine
-        query_result['game_id'] = game_id
+        for game in games_on_server:
+            game_id = game[0]
+            protocol = game[1]
 
-        if show_data:
-            print(query_result)
-        if hostname:
-            notify_master(hostname, query_result)
+            query_result = query_server(*protocol)
+
+            query_result['timestamp'] = int(time.time())
+            query_result['game_id'] = game_id
+
+            if show_data:
+                print(query_result)
+            if hostname:
+                notify_master(hostname, query_result)
+
         if not repeat_delay:
             exit(0)
         time.sleep(repeat_delay)
--- src/protocols.py
+++ src/protocols.py
@@ -1,5 +1,20 @@
 import subprocess
 
+default_game_settings = {
+    'ut2004' : ('gamespy', 7787),
+    'css' : ('a2s', 27015),
+    'tf2' : ('a2s', 27015),
+    'csgo' : ('a2s', 27015),
+    'kf' : ('gamespy', 7717),
+    'gmod-prop' : ('a2s', 27015),
+    'gmod-ttt' : ('a2s', 27015),
+    'mc' : ('gamespy', 25565),
+    #'pa' : ('???', 0),
+    #'cod4' : ('quake3', 0),
+    #'tm' : ('???', 0),
+    #'ns2' : ('a2s', 0), #Steam only?
+}
+
 def network_identity(port):
     data = {}
     command_ip = "ip addr | grep 'inet ' | awk '{print $2}' | cut -d/ -f1 | grep -v '127.' | head -n 1"
@@ -16,3 +31,5 @@ def a2s_query_server(port):
     data = {}
     data.update(network_identity(port))
     return data
+
+#Packet template
